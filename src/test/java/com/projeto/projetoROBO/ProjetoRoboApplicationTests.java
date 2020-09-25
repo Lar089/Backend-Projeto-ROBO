89f475316d4bf7524c1a68fc304748581c0f9850
Lar089 <40472514+Lar089@users.noreply.github.com>
2020-09-24 22:55:53 -0300

    Adicionei o projeto

---
+++ src/test/java/com/projeto/projetoROBO/ProjetoRoboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.projetoROBO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoRoboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
